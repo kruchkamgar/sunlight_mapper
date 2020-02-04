1f67183664f9e0a687cf48b3aaaa64984d4edeb0
kruchkamgar <kruchkamgar@gmail.com>
2020-02-04 14:46:38 -0700

    added Jest to test
    resolved annoying 'test suite' error in jest

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -3,3 +3,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+// skips annoying jest test suite error message
+test.skip('skip', () => {})
