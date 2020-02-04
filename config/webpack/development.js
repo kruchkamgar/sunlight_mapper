3c1eae1bc51d69617d063195e7ef91e235f18c1e
kruchkamgar <kruchkamgar@gmail.com>
2020-02-04 11:31:55 -0700

    add config

---
+++ config/webpack/development.js
@@ -0,0 +1,8 @@
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+const environment = require('./environment')
+
+environment.loaders.delete('nodeModules')
+
+module.exports = environment.toWebpackConfig()
