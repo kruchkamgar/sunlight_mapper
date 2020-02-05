97be44aaac30f1bc574cd412a61e49d712a79b98
kruchkamgar <kruchkamgar@gmail.com>
2020-02-05 15:56:35 -0700

    remove the nominalPower display, and style data panels

--- app/javascript/src/UI/components/Output.js
+++ app/javascript/src/UI/components/Output.js
@@ -4,8 +4,8 @@ const Output = ({nominalPower, coordinates}) => {
 
   return(
     <div className="output">
-      { nominalPower &&
-        <p className="data">nominal power: {nominalPower} </p> }
+      {/* nominalPower &&
+        <p className="data">nominal power: {nominalPower} </p> */}
       <p className="data subtext">
         {coordinates[0]}, {coordinates[1]}</p>
     </div>
