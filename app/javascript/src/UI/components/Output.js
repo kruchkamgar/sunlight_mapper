5050e9dc6328c33dc65d32afc8091336cc97b3bd
kruchkamgar <kruchkamgar@gmail.com>
2020-02-05 11:54:17 -0700

    adding output div

---
+++ app/javascript/src/UI/components/Output.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Output = ({nominalPower, coordinates}) => {
+
+  return(
+    <div className="output">
+      <p className="data">nominal power: {nominalPower}</p>
+      <p className="data subtext">
+        {coordinates[0]}, {coordinates[1]}</p>
+    </div>
+  )
+
+}
+
+export default Output
